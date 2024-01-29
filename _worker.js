c64eacea0a98a863db5c0f2386b594cd59939c43
gaterogue <158034120+gaterogue@users.noreply.github.com>
2024-01-29 18:05:43 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'a10072d3-c2c3-42cf-b7ce-89339ebd8efe';
 
-const proxyIPs = ['103.167.150.212'];
+const proxyIPs = ['163.181.19.152'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
